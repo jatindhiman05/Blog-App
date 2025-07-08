c65bb737020d69fb2f8c7404f3d71893d86b54b6
jatinn27 <jatin121dhiman@gmail.com>
2025-07-09 02:01:13 +0530

    delete blog controller controller fix

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -305,33 +305,39 @@ async function deleteBlog(req, res) {
         const blog = await Blog.findById(id);
 
         if (!blog) {
-            return res.status(500).json({
-                message: "Blog is not found",
-            });
+            return res.status(404).json({ message: "Blog not found" });
         }
 
-        if (creator != blog.creator) {
-            return res.status(500).json({
-                message: "You are not authorized for this action",
-            });
+        if (creator.toString() !== blog.creator.toString()) {
+            return res.status(403).json({ message: "Unauthorized to delete this blog" });
         }
 
-        await deleteImagefromCloudinary(blog.imageId);
+        if (blog.imageId) {
+            await deleteImagefromCloudinary(blog.imageId);
+        }
 
-        await Blog.findByIdAndDelete(id);
+        if (blog.content?.blocks) {
+            for (const block of blog.content.blocks) {
+                if (block.type === "image" && block.data?.file?.imageId) {
+                    try {
+                        await deleteImagefromCloudinary(block.data.file.imageId);
+                    } catch (err) {
+                        console.error(`Failed to delete image ${block.data.file.imageId}:`, err);
+                    }
+                }
+            }
+        }
+
         await User.findByIdAndUpdate(creator, { $pull: { blogs: id } });
 
-        return res.status(200).json({
-            success: true,
-            message: "Blog deleted successfully",
-        });
+        await Blog.findByIdAndDelete(id);
+
+        return res.status(200).json({ success: true, message: "Blog and all associated images deleted" });
     } catch (error) {
-        return res.status(500).json({
-            message: error.message,
-        });
+        console.error("Delete Blog Error:", error);
+        return res.status(500).json({ message: error.message });
     }
 }
-
 async function likeBlog(req, res) {
     try {
         const userId = req.user;
